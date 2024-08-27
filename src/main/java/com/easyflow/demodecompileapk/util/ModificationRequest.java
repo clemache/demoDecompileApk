1d00c5443818be14e1d8ce337ba420a970603396
Cristhian Lemache <clemache@asiste.com.ec>
2024-08-27 16:41:36 -0500

    add - ModificationRequest.java / Clase para manejar listas de cambios para diferentes archivos, tipos y tipos de cambios en los archivos.

---
+++ src/main/java/com/easyflow/demodecompileapk/util/ModificationRequest.java
@@ -0,0 +1,61 @@
+package com.easyflow.demodecompileapk.util;
+
+public class ModificationRequest {
+
+    private String nameFile;
+    private modificationType type;
+    private String oldValueOrVariable;
+    private String newValue;
+    private typeFile typeFile;
+
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+
+    public modificationType getType() {
+        return type;
+    }
+
+    public void setType(modificationType type) {
+        this.type = type;
+    }
+
+    public String getOldValueOrVariable() {
+        return oldValueOrVariable;
+    }
+
+    public void setOldValueOrVariable(String oldValueOrVariable) {
+        this.oldValueOrVariable = oldValueOrVariable;
+    }
+
+    public String getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(String newValue) {
+        this.newValue = newValue;
+    }
+
+    public ModificationRequest.typeFile getTypeFile() {
+        return typeFile;
+    }
+
+    public void setTypeFile(ModificationRequest.typeFile typeFile) {
+        this.typeFile = typeFile;
+    }
+
+    private enum modificationType {
+        REPLACEVALUE,
+        ADDLINE,
+        REPLACELINE,
+        REMOVELINEA
+    }
+    private enum typeFile {
+        XML,
+        SMALI
+    }
+}
